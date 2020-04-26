241fb42e4371f4e608bc8c3d1c8d0c5f20ebf01f
Áron Szentpéteri <szearon1@gmail.com>
2020-04-26 22:14:09 +0200

    branch A commit

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -82,8 +82,8 @@ public class GT4500 implements SpaceShip {
           firingSuccess = primaryTorpedoStore.fire(1) && secondaryTorpedoStore.fire(1);
         break;
     }
-
+	
     return firingSuccess;
   }
-
+  //Branch A comment
 }
